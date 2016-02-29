90ebc47ca3692e1cb605eca5763c20c001c5e023
emikohunt <ehunt@collarts.edu.au>
2016-02-29 23:45:14 +1100

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+$('a[href*=#]').click(function(event){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    event.preventDefault();
+});
+	
+	})
